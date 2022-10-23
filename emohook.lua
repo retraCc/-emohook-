aa490eab88d1fe4bd1b801dc870c7c381eb2efbb
retraCcc <100065385+retraCcc@users.noreply.github.com>
2022-10-23 15:32:35 -0400

    Update emohook.lua

--- emohook.lua
+++ emohook.lua
@@ -31,10 +31,10 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(173, 86, 10);
-    OutlineColor = Color3.fromRGB(50, 50, 50);
+    MainColor = Color3.fromRGB(24, 24, 37);
+    BackgroundColor = Color3.fromRGB(22, 22, 31);
+    AccentColor = Color3.fromRGB(86, 52, 140);
+    OutlineColor = Color3.fromRGB(48, 48, 48);
 
     Black = Color3.new(0, 0, 0);
 
