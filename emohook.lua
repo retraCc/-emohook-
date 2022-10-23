08a90c8335b057df3ef64deedbf736d1350f5073
retraCcc <100065385+retraCcc@users.noreply.github.com>
2022-10-23 02:10:10 -0400

    Update emohook.lua

--- emohook.lua
+++ emohook.lua
@@ -2160,7 +2160,7 @@ do
 
     local WatermarkOuter = Library:Create('Frame', {
         BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 750, 0, -25);
+        Position = UDim2.new(0, 200, 0, -25);
         Size = UDim2.new(0, 213, 0, 20);
         ZIndex = 200;
         Visible = false;
