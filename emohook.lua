8b823319a76fb6c02202e58f7e90c7caee5bd581
retraCcc <100065385+retraCcc@users.noreply.github.com>
2022-10-24 03:44:19 -0400

    Update emohook.lua

--- emohook.lua
+++ emohook.lua
@@ -2540,7 +2540,7 @@ function Library:CreateWindow(...)
             Position = UDim2.new(0, 0, 0, 0);
             Size = UDim2.new(1, 0, 1, -1);
             Text = Name;
-            TextSize = 13;
+            TextSize = 13.25;
             ZIndex = 1;
             Parent = TabButton;
         });
