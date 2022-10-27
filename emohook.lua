cffc751346bc0b8a32fdac0176af1fa280e28636
retraCcc <100065385+retraCcc@users.noreply.github.com>
2022-10-27 17:40:25 -0400

    Update emohook.lua

--- emohook.lua
+++ emohook.lua
@@ -1447,7 +1447,7 @@ do
             Rotation = -90;
             Transparency = NumberSequence.new({
 	        NumberSequenceKeypoint.new(0, 0),
-	        NumberSequenceKeypoint.new(1, 1)
+	        NumberSequenceKeypoint.new(1, 0.5)
         });
             Parent = ToggleInner;
         });
